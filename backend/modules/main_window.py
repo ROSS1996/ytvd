94d2a353390cead50a78e0ee656dc19cded64c0c
Robson Santana <41270745+ROSS1996@users.noreply.github.com>
2024-10-01 19:06:55 -0300

    Fix youtube url handler

--- backend/modules/main_window.py
+++ backend/modules/main_window.py
@@ -11,6 +11,7 @@ from .custom_web_engine_page import CustomWebEnginePage
 from .download_manager import DownloadManager
 import asyncio
 from .constants import CACHE_DIRECTORY
+from urllib.parse import urlparse, parse_qs, urlunparse
 
 
 class DownloadWorker(QObject):
@@ -54,6 +55,9 @@ class DownloadManagerHandler:
 
     def request_download(self, youtube_url, format_type, quality):
         """Pergunta ao usuário por confirmação antes de iniciar o download."""
+        # Tratando o youtube_url para manter apenas o parâmetro 'v'
+        youtube_url = self.clean_youtube_url(youtube_url)
+
         # Cria a mensagem de confirmação
         msg_box = QMessageBox(self.parent)
         msg_box.setWindowTitle(f'Confirmação de Download {format_type.capitalize()}')
@@ -69,6 +73,25 @@ class DownloadManagerHandler:
         # Verifica a resposta do usuário
         if msg_box.clickedButton() == btn_sim:
             self.parent.start_download(youtube_url, format_type, quality)
+
+    def clean_youtube_url(self, url):
+        """Extrai apenas o parâmetro 'v' de uma URL do YouTube."""
+        # Parseia a URL em seus componentes
+        parsed_url = urlparse(url)
+        
+        # Extrai os parâmetros da query
+        query_params = parse_qs(parsed_url.query)
+        
+        # Verifica se o parâmetro 'v' está presente
+        if 'v' in query_params:
+            video_id = query_params['v'][0]  # O ID do vídeo
+            # Reconstroi a URL com apenas o parâmetro 'v'
+            clean_url = urlunparse((parsed_url.scheme, parsed_url.netloc, parsed_url.path, '', f'v={video_id}', ''))
+            return clean_url
+        else:
+            # Se não houver o parâmetro 'v', retorna a URL original
+            return url
+
 class BrowserWindow(QMainWindow):
     """Janela Principal do Navegador com recursos de download de vídeos do YouTube."""
 
