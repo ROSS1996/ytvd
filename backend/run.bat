208bc24c8b8d7597acd5de8764608bb352d74e09
Robson Santana <41270745+ROSS1996@users.noreply.github.com>
2024-10-01 15:48:48 -0300

    Adjust bat scripts

--- backend/run.bat
+++ backend/run.bat
@@ -1,3 +1,7 @@
-
+@echo off
+cd /d D:\CustomYTdownloader\backend
+call venv\Scripts\activate.bat
 pip install yt-dlp -U
+pip install -r requirements.txt
 python app.py
+pause
---
+++ backend/venv.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d D:\CustomYTdownloader\backend
+python -m venv venv
+pause
